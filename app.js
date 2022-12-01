9a2bf3c08de3c4c188ac315cfa9cee07c70ac7b5
TMV <m.timarsuev@gmail.com>
2022-12-01 12:32:42 +0300

    Server basis

---
+++ app.js
@@ -0,0 +1,23 @@
+require(`dotenv`).config();
+
+const express = require(`express`);
+const app = express();
+
+require(`express-async-errors`);
+
+app.get(`/`, (req, res) =>
+  res.status(200).send(`We will build this API, we have the technology!`)
+);
+
+const port = process.env.PORT || 3000;
+
+void (async function start() {
+  try {
+    app.listen(port, () => {
+      console.log(`Application is up and running at port ${port}...`);
+    });
+  } catch (error) {
+    console.error(`Application error!`, error);
+    process.exit(1);
+  }
+})();
